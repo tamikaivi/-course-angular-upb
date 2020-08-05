7812a0132ac054b87b008e1506b898cb5481c9e2
tamikaivi <yukisaki1998@gmail.com>
2020-08-05 18:47:50 -0400

    observer

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {from, fromEvent } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'angular-course';
+
+  ngOnInit(){
+
+    const array = from([1,2,3,4,5,6]);
+    array.subscribe(s => console.log('Items', s))
+
+    const aux = fromEvent<MouseEvent>(document, 'mousemove');
+    aux.subscribe(s => console.log('event: ',s.clientX +', ' +s.clientY));
+
+
+  }
+}
