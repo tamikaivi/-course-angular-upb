bd8934cb0da6d92aa9e4534efc90684b187b308b
tamikaivi <yukisaki1998@gmail.com>
2020-08-21 15:42:33 -0400

    first child routering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HomeComponent} from './components/home/home.component'
+import {AdminComponent} from './components/admin/admin.component'
+import {AboutComponent} from './components/about/about.component'
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import {AppRoutingRoutes } from './app-routing.routing'
+import {Home1Component} from './components/home1/home1.component'
+import {Home2Component} from './components/home2/home2.component'
 @NgModule({
    declarations: [		
       AppComponent,
-   
+   Home1Component,
+   Home2Component,
+   HomeComponent,
+   AdminComponent,
+   AboutComponent
    ],
    imports: [
       BrowserModule,
