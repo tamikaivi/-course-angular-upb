b66182de0ae5da3127c20916a48185aaf19086f6
tamikaivi <yukisaki1998@gmail.com>
2020-08-24 15:12:17 -0400

    child in moduleroute

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import {UsecomComponent} from './usecom/usecom.component'
+import {User1Component} from './user1/user1.component'
+import {User2Component} from './user2/user2.component'
 
 @NgModule({
-  declarations: [UsecomComponent],
+  declarations: [UsecomComponent,
+    User1Component,
+    User2Component],
   imports: [
     CommonModule,
     UserRoutingModule
